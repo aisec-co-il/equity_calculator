b9525a2c746579bea7bcbc65c935b2f4ca3ac365
Vladimir Belykh <vladimir@hatcrab.com>
2025-06-10 11:19:36 +0300

    awesome fix

--- local.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# .env file (or set manually)
-DOCKER_BUILD_CONTEXT=./  # Build from current directory
-FLASK_ENV=development    # Enable debug mode
-
-# Start the service
-docker-compose up
